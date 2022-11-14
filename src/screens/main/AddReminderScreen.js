797d7ad54f4b80fae08ac0d353cdd613f9501fc4
trangj <joshua.trang@hotmail.com>
2022-11-14 14:53:18 -0500

    implement add reminder

--- src/screens/main/AddReminderScreen.js
+++ src/screens/main/AddReminderScreen.js
@@ -1,21 +1,77 @@
-import { StyleSheet } from 'react-native';
-import { View, Text } from 'react-native';
+import { Button, FormControl, Input, KeyboardAvoidingView, ScrollView, Text, VStack } from 'native-base';
+import { Controller, useForm } from 'react-hook-form';
+import { supabase } from '../../lib/supabase';
+import { useRemindersStore } from '../../stores/reminderStore';
 
-function AddReminderScreen() {
+function AddReminderScreen({ route, navigation }) {
+  const reminders = useRemindersStore(state => state.reminders);
+  const setReminders = useRemindersStore(state => state.setReminders);
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      description: '',
+    }
+  });
+
+  const onSubmit = async ({ description }) => {
+
+    if (!route.params.markerId) {
+      return;
+    }
+    
+    const { data, error } = await supabase
+    .from('reminder')
+    .insert([
+      { marker_id: route.params.markerId, description },
+    ])
+    .select('*')
+
+    setReminders([...reminders, ...data]);
+
+    if (error) {
+      Alert.alert(error.message)
+    } else {
+      navigation.navigate("MarkerDetails", {markerId: route.params.markerId})
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>View Groups!</Text>
-    </View>
+    <VStack h="full">
+      <ScrollView>
+        <VStack space="4" mt="16" p="4">
+          <Text fontSize="2xl" >Add Reminder</Text>
+          <FormControl isInvalid={errors.description}>
+            <FormControl.Label>Description</FormControl.Label>
+            <Controller
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  multiline
+                  numberOfLines={10}
+                  textAlignVertical="top"
+                />
+              )}
+              name="description"
+            />
+            <FormControl.ErrorMessage>
+              Reminder description is required.
+            </FormControl.ErrorMessage>
+          </FormControl>
+        </VStack>
+      </ScrollView>
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} keyboardVerticalOffset={100}>
+        <VStack mt="auto" space="2" p="4">
+          <Button onPress={handleSubmit(onSubmit)}>
+            Add Reminder
+          </Button>
+        </VStack>
+      </KeyboardAvoidingView>
+    </VStack>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default AddReminderScreen
--- src/screens/main/MarkerDetailsScreen.js
+++ src/screens/main/MarkerDetailsScreen.js
@@ -1,15 +1,18 @@
-import { Checkbox, CloseIcon, IconButton, Text } from 'native-base';
-import { useEffect, useState } from 'react';
+import { Box, Button, Checkbox, CloseIcon, IconButton, Text } from 'native-base';
+import { useEffect } from 'react';
+import { Alert } from 'react-native';
 import { FlatList } from 'react-native';
 import { supabase } from '../../lib/supabase';
+import { useRemindersStore } from '../../stores/reminderStore';
 import ListItem from '../../ui/ListItem';
 
-function MarkerDetailsScreen({ route }) {
-  const [reminders, setReminders] = useState([])
+function MarkerDetailsScreen({ route, navigation }) {
+  const reminders = useRemindersStore(state => state.reminders);
+  const setReminders = useRemindersStore(state => state.setReminders);
 
   useEffect(() => {
     async function fetchReminders() {
-      const {data, error} = await supabase.from('reminder').select('*').eq('id', route.params.markerId);
+      const {data, error} = await supabase.from('reminder').select('*').eq('marker_id', route.params.markerId);
       setReminders(data);
     }
 
@@ -25,38 +28,62 @@ function MarkerDetailsScreen({ route }) {
         completed_at: checked ? (new Date()).toISOString() : null,
       })
       .match({ id })
-      .select('*');
-    setReminders(data);
+      .select();
+    setReminders(
+      reminders.map(reminder => {
+        if (reminder.id === id) {
+          reminder.completed_at = data[0].completed_at;
+        }
+        return reminder;
+      })
+    );
   } 
 
   async function deleteReminder(id) {
-    const { data } = await supabase.from('reminder').delete().match({ id })
-    setReminders(data);
+    const { error } = await supabase.from('reminder').delete().match({ id })
+
+    if (error) {
+      Alert.alert(error.message)
+    } else {
+      setReminders(reminders.filter(reminder => reminder.id !== id));
+    }
   }
 
   return (
-    <FlatList
-      data={reminders}
-      keyExtractor={(reminder) => reminder.id}
-      renderItem={({item: reminder}) => (
-        <ListItem
-          justifyContent="space-between"
+    <>
+      <FlatList
+        data={reminders}
+        keyExtractor={(reminder) => reminder.id}
+        renderItem={({item: reminder}) => (
+          <ListItem
+            justifyContent="space-between"
+          >
+            <Checkbox
+              isChecked={!!reminder.completed_at}
+              accessibilityLabel="Reminder completion status"
+              onChange={checked => changeReminderStatus(reminder.id, checked)}
+            />
+            <Text>
+              {reminder.description}
+            </Text>
+            <IconButton 
+              icon={<CloseIcon/>}
+              onPress={() => deleteReminder(reminder.id)}
+            />
+          </ListItem>
+        )}
+      />
+      <Box
+        bg="white"
+        p="3"
+      >
+        <Button
+          onPress={() => navigation.navigate("AddReminder", {markerId: route.params.markerId})}
         >
-          <Checkbox
-            isChecked={!!reminder.completed_at}
-            accessibilityLabel="Reminder completion status"
-            onChange={checked => changeReminderStatus(reminder.id, checked)}
-          />
-          <Text>
-            {reminder.description}
-          </Text>
-          <IconButton 
-            icon={<CloseIcon/>}
-            onPress={() => deleteReminder(reminder.id)}
-          />
-        </ListItem>
-      )}
-    />
+          Add Reminder
+        </Button>
+      </Box>
+    </>
   )
 }
 
