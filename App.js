3006a44cc61500310f733a9d6c2dae6be5efac4e
trangj <joshua.trang@hotmail.com>
2023-01-07 22:08:05 -0500

    fix marker loading

--- App.js
+++ App.js
@@ -3,8 +3,42 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/navigators/AuthNavigator'
 import { NativeBaseProvider } from "native-base";
 import { SafeAreaView, StatusBar } from 'react-native';
+import { supabase } from './src/lib/supabase'
+import { useEffect } from 'react'
+import { useMarkerStore } from './src/stores/markerStore'
+import { useGroupStore } from './src/stores/groupStore'
+import { useSessionStore } from './src/stores/sessionStore'
+
 
 export default function App() {
+  const setSession = useSessionStore(state => state.setSession)
+  const setMarkers = useMarkerStore(state => state.setMarkers)
+  const group = useGroupStore(state => state.group);
+
+  // handle authentication on app load
+  useEffect(() => {
+    async function fetchSession() {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+
+      setMarkers([])
+
+      if (group) {
+        const {data, error} = await supabase
+          .from('marker')
+          .select('*')
+          .eq('group_id', group.id)
+
+        setMarkers(data);
+      }
+    }
+
+    fetchSession();
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     <SafeAreaView
       style={{
--- src/navigators/AuthNavigator.js
+++ src/navigators/AuthNavigator.js
@@ -3,40 +3,12 @@ import WelcomeScreen from '../screens/auth/WelcomeScreen'
 import LoginScreen from '../screens/auth/LoginScreen'
 import RegisterScreen from '../screens/auth/RegisterScreen'
 import DrawerNavigator from './DrawerNavigator'
-import { supabase } from '../lib/supabase'
-import { useEffect, useState } from 'react'
 import { useSessionStore } from '../stores/sessionStore'
-import { useMarkerStore } from '../stores/markerStore'
-import { useGroupStore } from '../stores/groupStore'
 
 const Stack = createNativeStackNavigator()
 
 function AuthNavigator() {
   const session = useSessionStore(state => state.session)
-  const setSession = useSessionStore(state => state.setSession)
-  const setMarkers = useMarkerStore(state => state.setMarkers)
-  const group = useGroupStore(state => state.group);
-
-  useEffect(() => {
-    async function fetchSession() {
-      const { data } = await supabase.auth.getSession();
-      setSession(data.session);
-
-      if (group) {
-        const {data, error} = await supabase
-          .from('marker')
-          .select('*')
-          .eq('group_id', group.id)
-
-        setMarkers(data);
-      }
-    }
-
-    fetchSession();
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
