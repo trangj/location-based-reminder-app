9a8cd6dcd2629a4c9d840d2e9a324bc27fbd7ba7
trangj <joshua.trang@hotmail.com>
2023-03-09 16:38:18 -0500

    update expo and restyle settings screen

--- src/screens/tabs/SettingsScreen.js
+++ src/screens/tabs/SettingsScreen.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from 'react-native';
-import { View } from 'react-native';
-import { Button, Text, useColorMode } from 'native-base'
+import { Divider, ScrollView, Text, useColorMode, VStack } from 'native-base'
 import { supabase } from '../../lib/supabase'
 import { useSessionStore } from '../../stores/sessionStore';
 import { useNavigation } from '@react-navigation/native';
+import ListItem from '../../ui/ListItem';
+import dayjs from 'dayjs';
 
 function SettingsScreen() {
   const user = useSessionStore(state => state.user)
@@ -11,22 +11,38 @@ function SettingsScreen() {
   const { toggleColorMode } = useColorMode();
 
   return (
-    <View style={styles.container}>
-      <Text>Signed in as {user.email}</Text>
-      <Button onPress={() => toggleColorMode()}>Change Theme</Button>
-      <Button onPress={() => navigation.navigate("ChangePasswordScreen")}>Change Password</Button>
-      <Button onPress={() => navigation.navigate("ChangeEmailScreen")}>Change Email</Button>
-      <Button onPress={() => supabase.auth.signOut()}>Logout</Button>
-    </View>
+    <ScrollView>
+      <VStack p="3">
+        <Text fontWeight="medium">
+            {user.email}
+        </Text>
+        <Text variant="alt">User since {dayjs(user.created_at).format('DD-MM-YYYY')}</Text>
+      </VStack>
+      <ListItem onPress={() => toggleColorMode()}>
+        <Text>
+          Change Theme
+        </Text>
+      </ListItem>
+      <Divider />
+      <ListItem onPress={() => navigation.navigate("ChangePasswordScreen")}>
+        <Text>
+          Change Password
+        </Text>
+      </ListItem>
+      <Divider />
+      <ListItem onPress={() => navigation.navigate("ChangeEmailScreen")}>
+        <Text>
+          Change Email
+        </Text>
+      </ListItem>
+      <Divider />
+      <ListItem onPress={() => supabase.auth.signOut()}>
+        <Text>
+        Logout
+        </Text>
+      </ListItem>
+    </ScrollView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default SettingsScreen
